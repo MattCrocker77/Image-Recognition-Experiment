d5839f2ec1405d104050d078aefdb318fc2981d1
Matt Crocker <mattcrocker@me.com>
2019-11-01 10:25:12 +0000

    First commit.

--- ImageRecognitionExperiment/ViewController.swift
+++ ImageRecognitionExperiment/ViewController.swift
@@ -9,17 +9,38 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    var visualEffect: NSVisualEffectView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.addVibrancy()
 
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    func addVibrancy() {
+        visualEffect = NSVisualEffectView()
+        visualEffect.translatesAutoresizingMaskIntoConstraints = false
+        visualEffect.state = .active
+        
+        // Default material is ".appearanceBased"
+        visualEffect.material = .fullScreenUI
+        
+        // The following is used to position a view relative to another (front to back).
+        // If relativeTo is nil then the view is placed behind all its siblings.
+        view.addSubview(visualEffect, positioned: NSWindow.OrderingMode.below, relativeTo: nil)
+        
+        // Ensure the NSVisualEffectView fills the window
+        visualEffect.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        visualEffect.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        visualEffect.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        visualEffect.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 
 
