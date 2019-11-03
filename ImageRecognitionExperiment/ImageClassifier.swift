25ab51e987bdccce6fdddddbd038335bca9c8405
Matt Crocker <mattcrocker@me.com>
2019-11-03 21:10:37 +0000

    Added classifier model as argument to classifier class initialiser.

--- ImageRecognitionExperiment/ImageClassifier.swift
+++ ImageRecognitionExperiment/ImageClassifier.swift
@@ -13,15 +13,36 @@ import Vision
 class ImageClassifier: NSObject {
 
     var classification: VNClassificationObservation!
+    var classifier: VNCoreMLModel!
+    
+    init(model: String = "Inceptionv3") {
+        super.init()
+        
+        // The "try" below has to be wrapped in a do/catch
+        do {
+            // Initialise the classifier with a pre-trained model
+            // For others see https://developer.apple.com/machine-learning/models/ and https://github.com/likedan/Awesome-CoreML-Models
+            switch model {
+            case "Inceptionv3":
+                classifier = try VNCoreMLModel(for: Inceptionv3().model)
+            case "MobileNetV2":
+                classifier = try VNCoreMLModel(for: MobileNetV2().model)
+            case "Resnet50":
+                classifier = try VNCoreMLModel(for: Resnet50().model)
+            case "VGG16":
+                classifier = try VNCoreMLModel(for: VGG16().model)
+            default:
+                fatalError("Model \"\(model)\" is not known.")
+            }
+        } catch  {
+            print("Unable to initialise classifier model due to error \"(\(error))\"")
+        }
+    }
     
     func classify(url: URL) -> VNClassificationObservation{
         
         // The "try" below has to be wrapped in a do/catch
         do {
-            // Initialise the model
-            // For others see https://github.com/likedan/Awesome-CoreML-Models
-            let classifier = try VNCoreMLModel(for: Inceptionv3().model)
-            
             // The following makes use of the Vision (VN) framework to avoid having to process the images explicitly
             let request = VNCoreMLRequest(model: classifier, completionHandler: self.handleResults)
             let handler = VNImageRequestHandler(url: url)
